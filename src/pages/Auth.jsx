79efc9cfbcc2a3eb32d5fb107b4d46e5b5239a40
Luis Escobar <luisaeniebla@gmail.com>
2026-09-18 13:38:57 -0700

    Added navbar

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,7 @@
+export default function Checkout() {
+  return (
+    <div className="">
+      <h1>Auth</h1>
+    </div>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Checkout() {
+  return (
+    <div className="">
+      <h1>Home</h1>
+    </div>
+  )
+}
